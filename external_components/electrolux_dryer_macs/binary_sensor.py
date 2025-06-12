a4cd667649caf47d30b2fd256d2dc64f00fd8402
Agócs Dániel <agocsdaniel@gmail.com>
2025-06-13 01:31:16 +0200

    It works

---
+++ external_components/electrolux_dryer_macs/binary_sensor.py
@@ -0,0 +1,43 @@
+import esphome.codegen as cg
+from esphome.components import binary_sensor
+import esphome.config_validation as cv
+from esphome.const import (
+    CONF_ID,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_RUNNING,
+)
+
+from . import CONF_ELECTROLUX_DRYER_MACS_ID, ElectroluxDryerMacsComponent
+
+CONF_POWERED_ON = "powered_on"
+CONF_RUNNING = "running"
+CONF_DELICATE_MODE = "delicate_mode"
+
+TYPES = [
+    CONF_POWERED_ON,
+    CONF_RUNNING,
+    CONF_DELICATE_MODE
+]
+
+CONFIG_SCHEMA = cv.All(
+    cv.Schema(
+        {
+            cv.GenerateID(CONF_ELECTROLUX_DRYER_MACS_ID): cv.use_id(ElectroluxDryerMacsComponent),
+            cv.Optional(CONF_POWERED_ON): binary_sensor.binary_sensor_schema(device_class=DEVICE_CLASS_POWER),
+            cv.Optional(CONF_RUNNING): binary_sensor.binary_sensor_schema(device_class=DEVICE_CLASS_RUNNING),
+            cv.Optional(CONF_DELICATE_MODE): binary_sensor.binary_sensor_schema(),
+        }
+    ).extend(cv.COMPONENT_SCHEMA)
+)
+
+
+async def setup_conf(config, key, hub):
+    if sensor_config := config.get(key):
+        var = await binary_sensor.new_binary_sensor(sensor_config)
+        cg.add(getattr(hub, f"set_{key}_binary_sensor")(var))
+
+
+async def to_code(config):
+    hub = await cg.get_variable(config[CONF_ELECTROLUX_DRYER_MACS_ID])
+    for key in TYPES:
+        await setup_conf(config, key, hub)
---
+++ external_components/electrolux_dryer_macs/electrolux_dryer_macs.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/components/uart/uart.h"
+#include "esphome/core/log.h"
+#include "esphome/core/defines.h"
+
+#include <vector>
+
+namespace esphome {
+namespace electrolux_dryer_macs {
+
+class ElectroluxDryerMacsComponent : public Component, public uart::UARTDevice {
+ public:
+  ElectroluxDryerMacsComponent(uart::UARTComponent *uart) : uart::UARTDevice(uart) {}
+
+#ifdef USE_SENSOR
+  SUB_SENSOR(remaining_time)
+  SUB_SENSOR(start_delay_time)
+  SUB_SENSOR(selected_program_number)
+  SUB_SENSOR(program_dryness_level)
+#endif
+
+#ifdef USE_TEXT_SENSOR
+  SUB_TEXT_SENSOR(selected_program_name)
+#endif
+
+#ifdef USE_BINARY_SENSOR
+  SUB_BINARY_SENSOR(powered_on)
+  SUB_BINARY_SENSOR(running)
+  SUB_BINARY_SENSOR(delicate_mode)
+#endif
+
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+  
+  // float get_setup_priority() const override { return esphome::setup_priority::HARDWARE_LATE; }
+
+  void set_crc_check(bool crc_check) { this->crc_check_ = crc_check; }
+  void set_receive_timeout(uint32_t receive_timeout) { this->receive_timeout_ = receive_timeout; }
+
+ protected:
+  void decode_data_(std::vector<uint8_t> data);
+  
+  std::vector<uint8_t> data_;
+  bool receiving_{false};
+  uint8_t data_count_{0};
+  uint32_t last_transmission_{0};
+  
+  uint32_t receive_timeout_{200};
+  bool crc_check_{false};
+
+};
+}  // namespace electrolux_dryer_macs
+}  // namespace esphome
+
